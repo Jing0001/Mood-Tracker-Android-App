d0daae26c7f160a89d83ec0484f759a571d3926d
Lujie <72049706+lujwen@users.noreply.github.com>
2022-10-22 01:40:50 -0700

    Add files via upload
    assignment7

---
+++ MobileApplicationTeam18/app/src/main/java/edu/northeastern/mobileapplicationteam18/MainActivity.java
@@ -0,0 +1,45 @@
+package edu.northeastern.mobileapplicationteam18;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button weather = findViewById(R.id.weather);
+        weather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Weather.class);
+                startActivity(intent);
+            }
+        });
+
+        Button database = findViewById(R.id.database);
+        database.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Realtime.class);
+                startActivity(intent);
+            }
+        });
+
+        Button moodTracker = findViewById(R.id.moodTracker);
+        moodTracker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Mood.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
