246bea423a25738cb60526094db29a25bfa453f8
Jing0001 <feng.jing1@northeastern.edu>
2022-11-20 14:05:20 -0800

    Modify Home Screen's Layout

--- MobileApplicationTeam18/app/src/main/java/edu/northeastern/mobileapplicationteam18/FMoods.java
+++ MobileApplicationTeam18/app/src/main/java/edu/northeastern/mobileapplicationteam18/FMoods.java
@@ -15,6 +15,9 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FMoods extends AppCompatActivity {
     GridLayout mainGrid;
     Integer count = 0;
@@ -37,43 +40,46 @@ public class FMoods extends AppCompatActivity {
         //setToggleEvent(mainGrid);
     }
 
-    private void setToggleEvent(GridLayout mainGrid) {
-        //Loop all child item of Main Grid
-        for (int i = 0; i < mainGrid.getChildCount(); i++) {
-            //You can see , all child item is CardView , so we just cast object to CardView
-            final CardView cardView = (CardView) mainGrid.getChildAt(i);
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
-                        //Change background color
-                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
-                        Toast.makeText(FMoods.this, "State : True", Toast.LENGTH_SHORT).show();
-
-                    } else {
-                        //Change background color
-                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
-                        Toast.makeText(FMoods.this, "State : False", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
-    }
+//    private void setToggleEvent(GridLayout mainGrid) {
+//        //Loop all child item of Main Grid
+//        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+//            //You can see , all child item is CardView , so we just cast object to CardView
+//            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+//            cardView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
+//                        //Change background color
+//                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
+//                        Toast.makeText(FMoods.this, "State : True", Toast.LENGTH_SHORT).show();
+//
+//                    } else {
+//                        //Change background color
+//                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+//                        Toast.makeText(FMoods.this, "State : False", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
+//        }
+//    }
 
     private void setSingleEvent(GridLayout mainGrid) {
         //Loop all child item of Main Grid
         for (int i = 0; i < mainGrid.getChildCount(); i++) {
             //You can see , all child item is CardView , so we just cast object to CardView
             CardView cardView = (CardView) mainGrid.getChildAt(i);
+            ArrayList<String> allMoods = new ArrayList<String>(Arrays.asList("Happy", "Sad", "Sleepy", "Angry", "Confused", "a",
+                    "b", "c", "d", "e", "h", "i"));
             final int finalI = i;
             cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
                     Intent intent = new Intent(FMoods.this,FM1.class);
-                    intent.putExtra("info","This is activity from card item index  "+finalI);
-                    startActivity(intent);
+                    intent.putExtra("info","Your Mood is  "+allMoods.get(finalI));
 
+
+                    startActivity(intent);
                 }
             });
         }
