b97f4e62837baffeabc533a63be49a4f6fc69d88
Lujie <wen.l@northeastern.edu>
2022-12-07 00:14:27 -0800

    generate summary works

--- MobileApplicationTeam18/app/src/main/java/edu/northeastern/mobileapplicationteam18/FMoodSummary.java
+++ MobileApplicationTeam18/app/src/main/java/edu/northeastern/mobileapplicationteam18/FMoodSummary.java
@@ -8,7 +8,15 @@ import android.widget.TextView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class FMoodSummary extends AppCompatActivity {
     String userName;
@@ -18,6 +26,12 @@ public class FMoodSummary extends AppCompatActivity {
     private TextView embarrNumTV;
     private TextView hystNumTV;
     private TextView fatiNumTV;
+
+    private TextView sumTV;
+
+    String[] biggest = {"0","0"};
+    String[] smallest = {"1", "1"};
+
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
 
@@ -31,17 +45,20 @@ public class FMoodSummary extends AppCompatActivity {
         embarrNumTV = (TextView) findViewById(R.id.embNumber);
         hystNumTV = (TextView) findViewById(R.id.hysNumber);
         fatiNumTV = (TextView) findViewById(R.id.fatNumber);
+
+        sumTV = (TextView) findViewById(R.id.summaryTV);
+
         Bundle extras = getIntent().getExtras();
         System.out.println("extras in moods: " + extras);
         if (extras != null) {
             userName = extras.getString("user_name");
         }
-
         readCountFromDB();
-
+        generateSummary();
     }
 
     // read mood counts from DB
+
     private void readCountFromDB() {
         databaseReference.child("FUser").child(userName).child("MoodCount").get().addOnCompleteListener((task) -> {
             HashMap<String, Long> countMap = (HashMap) task.getResult().getValue();
@@ -79,6 +96,32 @@ public class FMoodSummary extends AppCompatActivity {
     } // end of read mood count from db
 
     // generate mood summary
-//    private void readCountFromDB()
+    private void generateSummary(){
+
+        databaseReference.child("FUser").child(userName).child("MoodCount").get().addOnCompleteListener((task) -> {
+            HashMap<String, Long> countMap = (HashMap) task.getResult().getValue();
+            if (countMap != null){
+                String init = countMap.keySet().iterator().next();
+                biggest[0] = init;
+                biggest[1] = "-1";
+                smallest[0] = init;
+                smallest[1] = String.valueOf(Double.POSITIVE_INFINITY);
+
+                for(String mood : countMap.keySet()){
+                    if(countMap.get(mood) >= Long.parseLong(biggest[1])){
+                        Array.set(biggest, 0, mood);
+                        Array.set(biggest, 1,  countMap.get(mood) + "");
+                    }
+                    else{
+                        Array.set(smallest, 0, mood);
+                        Array.set(smallest, 1,  countMap.get(mood) + "");
+                    }
+                }
+                sumTV.setText("Most of the time, you felt " + biggest[0] + ", and you barely " + smallest[0]);
+            }else{
+                sumTV.setText("Start to track your mood.");
+            }
+        });
+    }
 
 }
